7a2ac0f33de6bd61ba7e948cf853628a7b299700
Nick Gronow <nick@webonlineventures.com>
2013-12-16 20:52:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ovapi::Application.config.secret_key_base = '31e7d4931fff7f7b16014874a0a3bc6e2049c19f398c3e36c694f202cfab2d104e9a6d59798131d1bfdebc3a00b6a7aaa88dd1befacddbee10df95832b12183c'
